dddabe04e7bdc8ee52211ef09a1135762bb5a49c
Alson Shareef <alson.shareef@gmail.com>
2019-01-23 15:46:27 +0800

    Finished multiply-ascii encoder

--- src/components/Multiply-Ascii/multiply-ascii.js
+++ src/components/Multiply-Ascii/multiply-ascii.js
@@ -1,3 +1,11 @@
 const multiplyAscii = (message) => {
-    
+    let output = []
+    for(let i = 0; i < message.length; i++){
+        let num = message[i].charCodeAt(0)
+        output.push(num * 2)
+    }
+    output = output.join('')
+    return output
 }
+
+// console.log(multiplyAscii('My name is Alson'))
