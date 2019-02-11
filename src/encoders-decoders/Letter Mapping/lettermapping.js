e17e31c523a028f73122efb2002eae9c56a1e19d
Alson Shareef <alson.shareef@gmail.com>
2019-02-11 18:09:43 +0800

    Added decodeKey for Letter Mapping

--- src/encoders-decoders/Letter Mapping/lettermapping.js
+++ src/encoders-decoders/Letter Mapping/lettermapping.js
@@ -1,15 +1,20 @@
-let encodeKey = require('./encoding-key-LM')
+let {encodeKey, decodeKey} = require('./encoding-key-LM')
 
 const mapEncoder = (message) => {
     let output = ''
 
     for(let i = 0; i < message.length; i++){
         output += encodeKey[message[i]]
+        output += ','
     }
 
     return output
 }
 
-// console.log(mapEncoder('Hi, my name is Alson'))
+console.log(mapEncoder('Hi, my name is Alson'))
 
-module.exports = {mapEncoder}
+const mapDecoder = (encodedString) => {
+    
+}
+
+module.exports = {mapEncoder, mapDecoder}
